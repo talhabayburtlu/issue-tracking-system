24867a1457bd52c1e469368fa3d17e82564d9894
talhabayburtlu <talha.bayburtlu@layermark.com>
2023-10-14 20:22:24 +0300

    Initial commit.

---
+++ issue-tracking-service/src/main/java/com/its/issuetrackingservice/IssueTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.its.issuetrackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingServiceApplication.class, args);
+	}
+
+}
---
+++ issue-tracking-service/src/test/java/com/its/issuetrackingservice/IssueTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.issuetrackingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
